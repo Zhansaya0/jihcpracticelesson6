19b4b83a5bc58a89101cb129fadd33b57bea58dd
Zhansaya0 <abdullaeva.zhansaya03@gmail.com>
2021-03-15 14:40:08 +0600

    'jihcpracticelesson6

--- app/src/main/java/com/e/jihcpracticelesson4/MainActivity.java
+++ app/src/main/java/com/e/jihcpracticelesson4/MainActivity.java
@@ -1,5 +1,4 @@
 package com.e.jihcpracticelesson4;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -11,13 +10,20 @@ public class MainActivity extends AppCompatActivity {
 
     Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0;
     Button btnPoint, btnMultiple, btnDivision, btnPlus, btnMinus, btnPercentage, btnEqual, btnC, btnPlusminus;
-    TextView tv_print_san;
+    TextView tv_print_san, tv_zapis;
+    String firstNumber, secondNumber, znak, zapisText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initViews();
+        sandarMethods();
+        PlusMinusCleanMethods();
+        functionMethods();
+    }
+    public void initViews(){
         btn0 = findViewById(R.id.btn0);
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
@@ -28,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         btn7 = findViewById(R.id.btn7);
         btn8 = findViewById(R.id.btn8);
         btn9 = findViewById(R.id.btn9);
+
         btnPlus = findViewById(R.id.btnPlus);
         btnMinus = findViewById(R.id.btnMinus);
         btnMultiple = findViewById(R.id.btnMultiple);
@@ -37,53 +44,56 @@ public class MainActivity extends AppCompatActivity {
         btnEqual = findViewById(R.id.btnEqual);
         btnC = findViewById(R.id.btnC);
         btnPlusminus = findViewById(R.id.btnPlusminus);
+
         tv_print_san = findViewById(R.id.tv_print_san);
-
+        tv_zapis = findViewById(R.id.tv_zapis);
+    }
+    public void sandarMethods(){
         View.OnClickListener buttonSandar = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String aldyngySan = tv_print_san.getText().toString();
-                String clickedNumber = "";
-                String result = "";
+        @Override
+        public void onClick(View view) {
+            String aldyngySan = tv_print_san.getText().toString();
+            String clickedNumber = "";
+            String result = "";
 
-                switch (view.getId()) {
-                    case R.id.btn0:
-                        clickedNumber = "0";
-                        break;
-                    case R.id.btn1:
-                        clickedNumber = "1";
-                        break;
-                    case R.id.btn2:
-                        clickedNumber = "2";
-                        break;
-                    case R.id.btn3:
-                        clickedNumber = "3";
-                        break;
-                    case R.id.btn4:
-                        clickedNumber = "4";
-                        break;
-                    case R.id.btn5:
-                        clickedNumber = "5";
-                        break;
-                    case R.id.btn6:
-                        clickedNumber = "6";
-                        break;
-                    case R.id.btn7:
-                        clickedNumber = "7";
-                        break;
-                    case R.id.btn8:
-                        clickedNumber = "8";
-                        break;
-                    case R.id.btn9:
-                        clickedNumber = "9";
-                        break;
-                }
-                if (!aldyngySan.equals("0")) result = aldyngySan + clickedNumber;
-                else result = clickedNumber;
-
-                tv_print_san.setText(result);
+            switch (view.getId()) {
+                case R.id.btn0:
+                    clickedNumber = "0";
+                    break;
+                case R.id.btn1:
+                    clickedNumber = "1";
+                    break;
+                case R.id.btn2:
+                    clickedNumber = "2";
+                    break;
+                case R.id.btn3:
+                    clickedNumber = "3";
+                    break;
+                case R.id.btn4:
+                    clickedNumber = "4";
+                    break;
+                case R.id.btn5:
+                    clickedNumber = "5";
+                    break;
+                case R.id.btn6:
+                    clickedNumber = "6";
+                    break;
+                case R.id.btn7:
+                    clickedNumber = "7";
+                    break;
+                case R.id.btn8:
+                    clickedNumber = "8";
+                    break;
+                case R.id.btn9:
+                    clickedNumber = "9";
+                    break;
             }
-        };
+            if (!aldyngySan.equals("0")) result = aldyngySan + clickedNumber;
+            else result = clickedNumber;
+
+            tv_print_san.setText(result);
+        }
+    };
         btn0.setOnClickListener(buttonSandar);
         btn1.setOnClickListener(buttonSandar);
         btn2.setOnClickListener(buttonSandar);
@@ -94,31 +104,84 @@ public class MainActivity extends AppCompatActivity {
         btn7.setOnClickListener(buttonSandar);
         btn8.setOnClickListener(buttonSandar);
         btn9.setOnClickListener(buttonSandar);
-
+    }
+    public void PlusMinusCleanMethods(){
         View.OnClickListener btnPlusMinusClean = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tv_print_san.setText("0");
-                switch (view.getId()){
+        @Override
+        public void onClick(View view) {
+            tv_print_san.setText("0");
+            switch (view.getId()){
 
-                    case R.id.btnC:
-                        tv_print_san.setText("0");
-                        break;
+                case R.id.btnC:
+                    tv_print_san.setText("0");
+                    tv_zapis.setText("");
+                    break;
 
-                    case R.id.btnPlusminus:
-                        String sanText = tv_print_san.getText().toString();
-                        int san = Integer.parseInt(sanText);
-                        tv_print_san.setText("0");
-                        if(san > 0) tv_print_san.setText("-"+sanText);
-                        else {
-                            san = san * (-1);
-                            tv_print_san.setText(""+san);
-                        };
-                        break;
-                }
+                case R.id.btnPlusminus:
+                    String sanText = tv_print_san.getText().toString();
+                    int san = Integer.parseInt(sanText);
+                    tv_print_san.setText("0");
+                    if(san > 0) tv_print_san.setText("-"+sanText);
+                    else {
+                        san = san * (-1);
+                        tv_print_san.setText(""+san);
+                    };
+                    break;
             }
-        };
+        }
+    };
         btnC.setOnClickListener(btnPlusMinusClean);
         btnPlusminus.setOnClickListener(btnPlusMinusClean);
+    }
+    public void functionMethods(){
+        View.OnClickListener btnFunctions = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            firstNumber = tv_print_san.getText().toString();
+            switch (v.getId()){
+                case R.id.btnPlus:
+                    znak = "+";
+                    break;
+                case R.id.btnMinus:
+                    znak = "-";
+                    break;
+                case R.id.btnMultiple:
+                    znak = "*";
+                    break;
+                case R.id.btnDivision:
+                    znak = "/";
+                    break;
+            }
+            zapisText = firstNumber + znak ;
+            tv_zapis.setText(zapisText);
+            tv_print_san.setText("0");
+        }
+    };
+        btnPlus.setOnClickListener(btnFunctions);
+        btnMinus.setOnClickListener(btnFunctions);
+        btnMultiple.setOnClickListener(btnFunctions);
+        btnDivision.setOnClickListener(btnFunctions);
+
+        btnEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                secondNumber = tv_print_san.getText().toString();
+                int san1 = Integer.parseInt(firstNumber);
+                int san2 = Integer.parseInt(secondNumber);
+                int res = 0;
+                if(znak.equals("+")){
+                    res = san1 + san2;
+                }else if(znak.equals("-")){
+                    res = san1 - san2;
+                }else if(znak.equals("*")){
+                    res = san1 * san2;
+                }else if(znak.equals("/")){
+                    res = san1 / san2;
+                }
+                zapisText = firstNumber + znak + secondNumber + "=" + res;
+                tv_zapis.setText(zapisText);
+                tv_print_san.setText(""+res);
+            }
+        });
     }
 }
